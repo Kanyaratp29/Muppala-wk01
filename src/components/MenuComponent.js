88aa75e76b9c322e1dd198503285d68cfcdb8316
Kanyarat Premprapapong <pigsweet1412@gmail.com>
2022-05-04 18:01:40 +0700

    W2-06 Single Pge Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+        CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+    
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>
+        );
+    }    
 
     class Menu extends Component {
 
@@ -50,15 +66,20 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
             return (
                 <div className="container">
                     <div className="row">
-                        {menu}
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>                
                     </div>
                     <div className="row">
-                      <div  className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                      </div>
+                        {menu}
                     </div>
                 </div>
-            );
+            );    
         }
     }
     
